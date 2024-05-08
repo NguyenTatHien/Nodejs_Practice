775627e7a0a945e97fc5916d813cb76dcef45cd0
NguyenTatHien <tathien2003@gmail.com>
2024-05-08 10:25:29 +0700

    Refactor code: create routes, app/controllers folders

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,19 @@
+class SiteController {
+
+    // [GET] /
+    home(req, res) {
+        res.render("home");
+    }
+
+    // [GET] /search
+    search(req, res) {
+        res.render("search");
+    }
+
+    // [GET] /contact
+    contact(req, res) {
+        res.render("contact");
+    }
+}
+
+module.exports = new SiteController;
