664de0d7000f9b21ab7e2ec53f1d36f38e276ed8
NguyenTatHien <tathien2003@gmail.com>
2024-05-08 11:47:42 +0700

    install husky, prettier, lint-staged

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,19 +1,18 @@
 class SiteController {
-
     // [GET] /
     home(req, res) {
-        res.render("home");
+        res.render('home');
     }
 
     // [GET] /search
     search(req, res) {
-        res.render("search");
+        res.render('search');
     }
 
     // [GET] /contact
     contact(req, res) {
-        res.render("contact");
+        res.render('contact');
     }
 }
 
-module.exports = new SiteController;
+module.exports = new SiteController();
