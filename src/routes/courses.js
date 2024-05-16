2d4ff2f7f09b77bef63a121af0c08a324b3c3b42
Nguyễn Hiển <tathien2003@gmail.com>
2024-05-16 15:16:45 +0700

    bai 31

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -8,6 +8,8 @@ router.get('/create', courseController.create);
 router.post('/store', courseController.store);
 router.get('/:id/edit', courseController.edit);
 router.put('/:id', courseController.update);
+router.delete('/:id', courseController.destroy);
+router.patch('/:id/restore', courseController.restore);
 router.get('/:slug', courseController.show);
 
 module.exports = router;
